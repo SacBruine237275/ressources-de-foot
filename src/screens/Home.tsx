6329030c55c39d4f1aff76d61b52623372993d67
SacBruine237275 <72806803+SacBruine237275@users.noreply.github.com>
2023-10-10 14:13:57 +0200

    Ajout de la liste dynamiquement avec la requete sparql

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,55 +2,44 @@ import { SearchBar } from "../components/SearchBar";
 import { Link } from 'react-router-dom';
 import { Club } from "./Club";
 import { WBK } from 'wikibase-sdk'
+import { useState,useEffect } from "react";
 
-
-async function getClubName(){
-  var requete:string=`
-PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-PREFIX wd: <http://www.wikidata.org/entity/>
-
-SELECT ?club (SAMPLE(?nom) AS ?nom) {
-  ?club wdt:P118 wd:Q9448 ;
-        wdt:P31 wd:Q476028 .
-  ?club wdt:P1448 ?nom
-}
-GROUP BY ?club`;
-  const wbk = WBK({
-    instance: 'https://www.wikidata.org',
-    sparqlEndpoint: 'https://query.wikidata.org/sparql'
-  })
-  const url:string = wbk.sparqlQuery(requete)
-  const simplifiedResults:any = await fetch(url)
-  .then(res => res.json())
-  console.log(simplifiedResults)
-}
 export const Home=()=>{
-  getClubName();
+  const [DataClubName,setDataClubName]=useState([]);
+  useEffect(() => {
+     const getClubName=async()=>{
+      var requete:string=`
+    PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+    PREFIX wd: <http://www.wikidata.org/entity/>
+    
+    SELECT ?club (SAMPLE(?nom) AS ?nom) {
+      ?club wdt:P118 wd:Q9448 ;
+            wdt:P31 wd:Q476028 .
+      ?club wdt:P1448 ?nom
+    }
+    GROUP BY ?club`;
+      const wbk = WBK({
+        instance: 'https://www.wikidata.org',
+        sparqlEndpoint: 'https://query.wikidata.org/sparql'
+      })
+      const url:string = wbk.sparqlQuery(requete)
+      const simplifiedResults:any = await fetch(url)
+      .then(res => res.json())
+      console.log(simplifiedResults)
+      setDataClubName(simplifiedResults.results.bindings)
+    }
+    getClubName();
+  });
     return(
         <div>
         <h1>Test page d'acceuil</h1>
         <SearchBar/>
         <ul>
-        <li><Link to={`/Club/Tottenham`}>Tottenham</Link></li>
-        <li><Link to={`/Club/Asernal`}>Asernal</Link></li>
-        <li><Link to={`/Club/City`}>Manchester City</Link></li>
-        <li><Link to={`/Club/Liverpool`}>Liverpool</Link></li>
-        <li><Link to={`/Club/AstonVilla`}>Aston Villa</Link></li>
-        <li><Link to={`/Club/Brighton`}>Brighton</Link></li>
-        <li><Link to={`/Club/WestHam`}>West Ham</Link></li>
-        <li><Link to={`/Club/Newcastle`}>Newcastle</Link></li>
-        <li><Link to={`/Club/CrystalPalace`}>Crystal Palace</Link></li>
-        <li><Link to={`/Club/United`}>Manchester United</Link></li>
-        <li><Link to={`/Club/Chelsea`}>Chelsea</Link></li>
-        <li><Link to={`/Club/Fulham`}>Fulham</Link></li>
-        <li><Link to={`/Club/NottmForest`}>Nottm Forest</Link></li>
-        <li><Link to={`/Club/Wolves`}>Wolves</Link></li>
-        <li><Link to={`/Club/Brentford`}>Brentford</Link></li>
-        <li><Link to={`/Club/Everton`}>Everton</Link></li>
-        <li><Link to={`/Club/LuttonTown`}>Lutton Town</Link></li>
-        <li><Link to={`/Club/Brunley`}>Brunley</Link></li>
-        <li><Link to={`/Club/Bournemouth`}>Bournemouth</Link></li>
-        <li><Link to={`/Club/SheffieldUnited`}>Sheffield United</Link></li>
+        {DataClubName.map((clubData: any, index: number) => {
+  return (
+    <li><Link to={`/Club/${clubData.nom.value} `}>{clubData.nom.value}</Link></li>
+  );
+})}
         </ul>
       </div>
     )
