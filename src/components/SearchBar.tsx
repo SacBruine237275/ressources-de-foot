699f37ddc7db8950b31d4728fd4c9c5d78572d8b
SacBruine237275 <72806803+SacBruine237275@users.noreply.github.com>
2023-10-17 09:17:32 +0200

    Mise en place de la SearchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,18 @@
-import callWikidataAPI from "../Common/Function"
-//chose qu'on vas réutilser à plusieurs endroit par exemple la barre de recherche qu'on voudras surement mettre sur plusieurs écrans mais pas forcement au même endroit
-export const SearchBar = () => {
+import React, { useState, useEffect } from "react";
+import callWikidataAPI from "../Common/Function";
+import { Link } from 'react-router-dom';
 
-    const inputChange = async (event: any) => {
-        var text = event.target.value;
-        if (text !== "") {
-            var requete: string = `
+
+export const SearchBar = () => {
+  const [DataSearchBar, setDataSearchBar] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    const inputChange = async () => {
+      if (searchText === "")
+        setDataSearchBar([]);
+      if (searchText !== "") {
+        const requete = `
             PREFIX wdt: <http://www.wikidata.org/prop/direct/>
             PREFIX wd: <http://www.wikidata.org/entity/>
             SELECT ?joueur ?name ?club ?nom {
@@ -13,24 +20,45 @@ export const SearchBar = () => {
                   ?joueur wdt:P54 ?club .
                   ?club wdt:P118 wd:Q9448 .
                   ?joueur wdt:P1559 ?name .
-                  FILTER(contains(?name, "${text}")) .
+                  FILTER(contains(?name, "${searchText}")) .
                 }
                 UNION
                 {
                   ?club wdt:P118 wd:Q9448 ;
                         wdt:P31 wd:Q476028 .
                   ?club wdt:P1448 ?nom .
-                  FILTER(contains(?nom, "${text}"))
+                  FILTER(contains(?nom, "${searchText}"))
                 }
-              }
-              
-            `
-            var temp = await callWikidataAPI(requete)
-        }
-    }
-    return (
-        <div>
-            <input type="text" placeholder="Rechercher un joueur ou un club" onChange={inputChange} />
-        </div>
-    )
-}
+              }`;
+        const data = await callWikidataAPI(requete);
+        setDataSearchBar(data.results.bindings);
+      }
+    };
+
+    inputChange();
+  }, [searchText]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Rechercher un joueur ou un club"
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <ul>
+        {DataSearchBar.map((result: any, index) => {
+          var isJoueur: boolean = !!result.joueur;
+          const partsOfUrl = isJoueur ? result.joueur?.value.split("/") : result.club?.value.split("/");
+          var endString: string = partsOfUrl[partsOfUrl.length - 1];
+          var link = isJoueur ? `/joueur/${endString}` : `/club/${endString}`
+          var text = isJoueur ? result.name?.value : result.nom?.value;
+          return (
+            <li>
+              <Link to={link}>{text}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
