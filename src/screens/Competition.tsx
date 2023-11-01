948e6b2fab3b63e71d31d5fa0ef86f2eee433d2d
SacBruine <gregorytacquet@gmail.com>
2023-11-01 14:20:30 +0100

    Suppresion de la page Competition.tsx

--- src/screens/Competition.tsx
+++
@@ -1,9 +0,0 @@
-type CompetitionProps = {
-    id: string
-}
-
-export const Competition: React.FC<CompetitionProps> = ({ id }) => {
-    return (
-        <div>Je suis une competition avec l'id {id}</div>
-    )
-}
