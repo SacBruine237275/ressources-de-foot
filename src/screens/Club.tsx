a6ef5200f89b9cd9ce963bed8b3e298bbe043ddc
SacBruine237275 <72806803+SacBruine237275@users.noreply.github.com>
2023-11-07 11:26:17 +0100

    Suppresion des éléments possible à mettre dans Club.tsx

--- src/screens/Club.tsx
+++ src/screens/Club.tsx
@@ -145,16 +145,6 @@ export const Club = () => {
                     </button>
                 </div>
             </div>
-            <p>Elements qu'on peux mettre : </p>
-            <ul>
-                <li>Emplacement du club (il a un genre de google map dans les données pour club très connus)</li>
-                <li>Si le club est déja redescendu ou sont historique</li>
-                <li>Nom du stade (pk pas des data dessus)</li>
-                <li>Les trophée gagner (élément important)</li>
-                <li>Date de création</li>
-                <li>Sponsor ???</li>
-                <li>Musique d'entré des joueurs si ça existe (pas présent dans tous les club et vraiment pas important)</li>
-            </ul>
         </div>
     )
 }
